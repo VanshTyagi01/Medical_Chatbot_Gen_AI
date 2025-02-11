51c310dace5273562b84fe7c356cc497f60b29a8
Vansh Tyagi <vanshtyagi011@outlook.com>
2025-02-11 21:45:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = "Medbot_project",
+    version= "0.0.0",
+    author = "Vansh Tyagi",
+    author_email = "vanshtyagi011@outlook.com",
+    packages=find_packages(),
+    install_requires=[]
+)
